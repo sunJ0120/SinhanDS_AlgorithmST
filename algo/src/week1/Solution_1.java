45d00a8d6137bcebf1104ddc4f7c3b6a261b1b95
sunJ0120 <sspure1214@gmail.com>
2025-06-03 10:00:30 +0900

    Solved: 프로그래머스 - 올바른 괄호 - 스택 두개를 이용해서 풀었으나, 하나 + FLAG로도 풀이 가능 - 스택 없이 CNT로 체크해서 푸는 깔끔한 풀이 방식도 존재

---
+++ algo/src/week1/Solution_1.java
@@ -0,0 +1,47 @@
+package week1;
+
+import java.util.Stack;
+
+public class Solution_1 {
+
+    public static void main(String[] args) {
+        //이거 모든 입력을 한번에 체크하려면...
+        String[] stList = {"()()","(())()",")()(","(()(","(()))"};
+        for(String st : stList){
+            System.out.println(solution(st));
+        }
+    }
+    public static boolean solution(String s) {
+        boolean answer = true;
+
+        char[] par = s.toCharArray();
+
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+
+        for(char c : par){
+            left.push(c);
+        }
+
+        while(!left.isEmpty()){
+            if(left.peek() == ')'){ //right의 괄호가 들어가 있음
+                right.push(left.pop());
+            }else{ //(일 경우
+                if(left.peek() == '(' && !right.isEmpty() && right.peek() == ')'){ //짝이 맞음
+                    left.pop();
+                    right.pop();
+                }else{ //짝이 안맞음
+                    answer = false;
+                    break;
+                }
+            }
+        }
+
+        //오른쪽이 남을 경우, 이것도 false로 써야한다.
+        if(!right.isEmpty()){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
