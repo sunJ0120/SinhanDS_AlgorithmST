bbb2452011e377200b6c6f5b604f50b5287a174d
sunJ0120 <sspure1214@gmail.com>
2025-07-20 11:52:32 +0900

    [week 4-1] Solved : 1715. 카드 정렬하기 풀이 완료 - 우선순위큐를 사용하는 문제라는걸 떠올리기 쉽지 않을 것 같다. 나는 유형별로 푼거라 풀었지만 이거 그냥 맞닥뜨리면 무조건 sort 부터 떠올릴듯... - 우선순위큐 제네릭의 존재를 처음 알았음..제네릭 사용하면 아름다운 풀이가 나온다. - 비교하는거 더할 때 root를 꺼내는게 아니고 꺼내진 두 더해진 값을 더한다. - 제네릭 사용하는거 지피티 도움 받은거라 꼭 복습 필요..

--- algo/src/week4/Main1715.java
+++ algo/src/week4/Main1715.java
@@ -3,87 +3,24 @@ package week4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /*
-BOJ 1715. 카드 정렬하기 1차 시도)
-- 기본적으로 합이 작은게 우선으로 더해져야 하므로, 최소힙을 따른다.
-- 사실 이 방법이 아닌거 같기도 한데... 딱히 생각나는 방법이 없어서 우선 진행한다.
-
-1. 우선 내림차순 정렬하고 배열에 저장한다.
-2. SIZE 1일때가지 진행한다.
-    1) delete 두 번 진행해서 두 값 더한다.
-    2) insert로 해당 값 더한다.
+BOJ 1715. 카드 정렬하기 2차 시도)
+- 있는 PriorityQueue 사용하는 방법을 익힘
+- PriorityQueue 사용법 지피티 참고 했으므로 반드시 복습 필요..
  */
 public class Main1715{
-    static List<Integer> heap;
-    static int heapSize;
 
-    //heap insert method
-    public static void insertHeap(int n){
-        heap.add(++heapSize, n);
-        int ind = heapSize;
-
-        //n이 더 작을 경우에 바꿔야 한다.
-        //n이 더 커지면 올라가지 않고 그 자리를 유지한다.
-        while(ind != 1 && n < heap.get(ind/2)){
-            heap.add(ind, heap.get(ind/2)); //부모의 요소를 아래로 내린다.
-            ind /= 2; //위로 올라간다.
-        }
-        heap.add(ind, n);
-    }
-
-    //child 값 비교를 위한 method
-    //더 값이 작은 child의 ind를 내보낸다.
-    public static int compareChild(int childInd1, int childInd2){
-        if(heap.get(childInd1) < heap.get(childInd2)){
-            return childInd1;
-        }
-        return childInd2;
-    }
-
-    //heap delete method
-    public static int deleteHeap(){
-        int removed = heap.get(1);
-        int root = heap.get(heapSize--); //맨 끝에 있는 요소
-
-        int parentInd = 1;
-        int childInd = 2;
-
-        while(childInd <= heapSize){
-            //child가 존재할 경우, 더 작은 인덱스를 구해야 한다.
-            if(childInd < heapSize){
-                childInd = compareChild(childInd, childInd+1);
-            }
-
-            // root가 더 작으면 그자리 유지 이므로,,
-            if(root < heap.get(childInd)){
-                break;
-            }
-
-            heap.add(parentInd,heap.get(childInd)); //올리기, 더 작으므로
-            //내려가기
-            parentInd = childInd;
-            childInd *= 2;
-        }
-
-        heap.add(parentInd, root);
-        return removed;
-    }
-
-    //heapSize가 1이면 멈춘다.
-    public static int cardSort(){
+    public static int cardSort(PriorityQueue<Integer> pq){
         int ans = 0;
         //heapSize가 1이면 멈춰야 한다.
-        while(heapSize > 1){
+        while(pq.size() > 1){
             //가장 작은 두개를 더해서 insert 한다.
-            int card1 = deleteHeap();
-            int card2 = deleteHeap();
-
-            insertHeap(card1 + card2);
-            ans += heap.get(1); //root 더하기
+            int a = pq.poll();
+            int b = pq.poll();
+            ans += (a + b); //위에서 뺀다고 보장 되어 있는게 아니므로, 이렇게 진행
+            pq.add(a + b);
         }
         return ans;
     }
@@ -91,15 +28,13 @@ public class Main1715{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        heapSize = num; //index
-        heap = new ArrayList<>();
-        heap.add(0); //0에 인덱스 추가
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i = 0; i<num; i++){
-            heap.add(Integer.parseInt(br.readLine()));
+            pq.add(Integer.parseInt(br.readLine()));
         }
 
-        Collections.sort(heap); //오름차순 정렬
-        System.out.println(cardSort());
+        System.out.println(cardSort(pq));
     }
 }
