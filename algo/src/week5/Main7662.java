a59f9eda1d761131f92cfd13fac213e682ff1ab7
sunJ0120 <sspure1214@gmail.com>
2025-07-25 23:56:08 +0900

    [week 5-3] Solved : 7662. 이중 우선순위 큐 - 이중 우선순위 큐의 경우, 우선순위 큐를 이용하면 remove 하는데 O(n^2)의 시간이 걸려서 우선순위큐를 사용할 수 없다. - 처음에는 우선순위 큐를 두 개 두고, 하나를 최소힙 하나를 최대힙 기반으로 두고 해결하고자 했으나, remove를 사용하지 않고 두 큐를 동기화 할 방법이 없어서 실패했다. - TreeMap이라는 것의 존재를 처음 알았는데, 이것을 이용하면 최소 최대 삭제를 O(N)의 시간안에 해결할 수 있다고 한다. firstKey, lastKey를 이용해서 해결하는데 사실 아직 TreeMap이 왜 추적 시간이 O(N) 밖에 안 걸리는지 잘 안와닿는다... 그렇다고 트리의 높이만큼만 탐색해서 그런것도 아닌 것 같고, 이건 추가 공부가 필요한 것 같다. - 이 문제의 경우, TreeMap을 사용하는 것 자체도 도움을 받았고, 특히 TreeMap에서 index에 값들을 두고 value에 크기를 둔다는 것 자체도 문서 도움을 받아서 반드시 반드시 추가 복습이 필요하다git add .git add .git add . 내 힘으로 푼 문제가 아니고 솔직히 너무 어렵다.....

--- algo/src/week5/Main7662.java
+++ algo/src/week5/Main7662.java
@@ -1,9 +1,8 @@
 package week5;
 
 import java.io.*;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 /**
  * [G5] 7662. 이중우선순위큐
@@ -20,43 +19,48 @@ public class Main7662 {
         for(int i = 0; i<roop; i++){
             //roop안에 돌아갈 num
             int num = Integer.parseInt(br.readLine());
+            //TreeMap을 선언
+            TreeMap<Integer, Integer> treeQueue = new TreeMap<>();
+            StringTokenizer st;
 
-            PriorityQueue<Integer> pqMin = new PriorityQueue<>(); //최소힙 (기본이 오름차순이다.)
-            //이거 최대힙으로 내림차순 하기 위해서는 Comparator.reverseOrder() 하는 것이 중요하다.
-            PriorityQueue<Integer> pqMax = new PriorityQueue<>(Comparator.reverseOrder()); //최대힙
+            for(int j = 0; j<num; j++){
+                st = new StringTokenizer(br.readLine());
+                String comm = st.nextToken();
+                int n = Integer.parseInt(st.nextToken());
 
-            for(int j = 0; j<num; j++) {
-                //두개씩 끊으니까 StringTokenizer st 선언~!
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                while (st.hasMoreTokens()) {
-                    String command = st.nextToken();
-                    int number = Integer.parseInt(st.nextToken());
+                //I일 경우, 더한다.
+                if(comm.equals("I")){
+                    //treeQueue.containsKey일 경우에 하나 올린다.
+                    //없을 경우 초기값 1을 배정
+                    treeQueue.put(n, treeQueue.containsKey(n) ? treeQueue.get(n)+1 : 1);
+                }else{ //comm.equals("D")일 경우, 삭제한다.
 
-                    if (command.equals("I")) {
-                        //I일 경우, 둘 다 더한다. 그래야 최소 최대 결정이 가능하다.
-                        pqMin.add(number);
-                        pqMax.add(number);
-                    } else { //command.equals("D") 삭제 연산
-                        if(pqMin.isEmpty() || pqMax.isEmpty()){
-                            bw.write("EMPTY" + "\n");
-                            break;
-                        }
-                        if (number == -1) {
-                            //최솟값을 삭제한다.
-                            int min = pqMin.poll();
-                            pqMax.remove(min); //같이 삭제한다 -> 시간초과 발생
-                        } else {
-                            //D 1 최댓값을 삭제한다.
-                            int max = pqMax.poll();
-                            pqMin.remove(max);
-                        }
+                    //근데 아무리 그래도 삭제인데 empty이면 이렇게 남겨두는게 맞지 않나...
+                    if(treeQueue.isEmpty()){
+                        continue;
+                    }
+
+                    int key;
+                    //비지 않고 삭제할 것이 있는 경우,
+                    if(n == -1){ //최솟값을 하나 줄인다.
+                        key = treeQueue.firstKey(); //최솟값을 하나 꺼낸다.
+                    }else{ //n == 1 일 경우 최댓값을 하나 줄인다.
+                        key = treeQueue.lastKey(); //최댓값을 하나 꺼낸다.
+                    }
+
+                    int cnt = treeQueue.get(key);
+                    treeQueue.remove(key);
+                    //cnt가 1개 이상일 경우, 갯수를 하나 줄이고 다시 넣어야 한다.
+                    if(cnt > 1){
+                        treeQueue.put(key, cnt-1);
                     }
                 }
             }
-            //이거 좀 안깔끔한데..어케 해야 하징 고민중....
-            if(!pqMin.isEmpty() || !pqMax.isEmpty()){
-                //둘 다 empty가 아닌 경우, max랑 min을 둘 다 내보낸다.
-                bw.write(pqMax.poll() + " " + pqMin.poll() + "\n");
+
+            if(treeQueue.isEmpty()){
+                bw.write("EMPTY" + "\n");
+            }else{ //EMPTY가 아닐 경우에만 bw.write로 내보낸다.
+                bw.write( treeQueue.lastKey() + " " + treeQueue.firstKey()+ "\n");
             }
         }
         bw.flush(); //쌓인걸 내보낸다.
