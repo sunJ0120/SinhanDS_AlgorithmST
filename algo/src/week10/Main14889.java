6001dff51fa5efe7469825ed2ab967367ed809ce
sunJ0120 <sspure1214@gmail.com>
2025-08-28 17:22:44 +0900

    [week 10-3.2] Refactor : 14889. 스타트와 링크 - 다음의 부분들 리팩터링을 진행하였다. 1. sum 관련 중복되는 로직을 pairSum()으로 따로 분리하였다. 2. set.contains()는 시간 복잡도가 매번 O(n)이므로, 이를 boolean[] 배열로 체크하도록 만들어서 해결하였다. 3. idx라는 파라미터는 조합 완탐에서 사용하지 않는 파라미터 이므로, 제거하였다.

--- algo/src/week10/Main14889.java
+++ algo/src/week10/Main14889.java
@@ -8,11 +8,26 @@ import java.util.*;
 public class Main14889 {
     static int ans;
     static List<Integer> set;
+    // contain을 안 쓰기 위해서 다음과 같이 boolean list를 하나 생성한다.
+    static boolean[] picked;
+    //이걸 사용하면 이 안에 이미 사용된 인덱스를 넣어둘 수 있다.
 
+    // 리스트의 합
+    public static int pairSum(List<Integer> set, int[][] team){
+        int ans = 0;
+        for(int i = 0; i<set.size(); i++){
+            for(int j = i+1; j<set.size(); j++){
+                ans += (team[set.get(i)][set.get(j)] + team[set.get(j)][set.get(i)]);
+            }
+        }
+        return ans;
+    }
+
+    // 여집합 뽑아내는 method
     public static List<Integer> another(int size){
         List<Integer> anot = new ArrayList<>();
         for(int i = 0; i<size; i++){
-            if(!set.contains(i)){
+            if(!picked[i]){ //false일 경우 진행한다.
                 anot.add(i);
             }
         }
@@ -21,47 +36,38 @@ public class Main14889 {
 
     // n/2개씩 조합을 구한다.
     // size는 n으로 둔다.
-    public static void n2(int start, int size, int idx, int[][] team){
+    public static void n2(int start, int size, int[][] team){
         if(set.size() == size/2){
             List<Integer> anot = another(size);
             //합을 구한다.
             //여집합의 경우, 모든 조합이 정해진 다음에 구하는 방식으로 가야 중복이 없음.
-            int ans1 = 0;
-            for(int i = 0; i<set.size(); i++){
-                for(int j = i+1; j<set.size(); j++){
-                    ans1 += (team[set.get(i)][set.get(j)] + team[set.get(j)][set.get(i)]);
-                }
-            }
-
-            int ans2 = 0;
-            for(int i = 0; i<anot.size(); i++){
-                for(int j = i+1; j<anot.size(); j++){
-                    ans2 += (team[anot.get(i)][anot.get(j)] + team[anot.get(j)][anot.get(i)]);
-
-                }
-            }
+            int ans1 = pairSum(set, team);
+            int ans2 = pairSum(anot, team);
 
             ans = Math.min(ans, Math.abs(ans2-ans1));
             return;
         }
         for(int i = start; i<size; i++){
             set.add(i);
-            n2(i+1, size, idx+1, team);
+            picked[i] = true; //사용된 인덱스 라는 것을 표시해준다.
+            n2(i+1, size, team);
 
             set.remove(set.size()-1);
+            picked[i] = false; //picked를 초기화 해야 하므로, 이걸 false로 바꿔주면 된다.
         }
     }
 
-    public static void init(){
+    public static void init(int num){
         ans = Integer.MAX_VALUE;
         set = new ArrayList<>();
+        picked = new boolean[num];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
 
-        init(); //static method들 초기화
+        init(num); //static method들 초기화
 
         //팀 넣기
         int[][] team = new int[num][num];
@@ -74,7 +80,7 @@ public class Main14889 {
         }
 
         // n/2 개씩 나오는 조합을 구한다.
-        n2(0, num, 0, team);
+        n2(0, num, team);
 
         //답
         System.out.println(ans);
